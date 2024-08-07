7926414e73eb993174b95e71b2aefec3c79dca61
Aniket2659 <aniket2659@gmail.com>
2024-08-07 15:52:11 +0530

    [Aniket] Add: Problem of covid19 database : To find out the death percentage locally and globally

---
+++ Covid_19/Query1.sql
@@ -0,0 +1,43 @@
+use covid_19;
+
+-- 1. To find out the death percentage locally and globally
+WITH joined_table AS (
+    SELECT 
+        cl.`Country/Region`, 
+        cl.Confirmed, 
+        cl.Deaths, 
+        cl.Recovered, 
+        cl.Active, 
+        cc.Date, 
+        wd.Population, 
+        wd.Continent
+    FROM country_wise_latest cl
+    INNER JOIN covid_19_clean_complete cc ON cl.`Country/Region` = cc.`Country/Region`
+    INNER JOIN full_grouped fg ON cl.`Country/Region` = fg.`Country/Region`
+    INNER JOIN worldometer_data wd ON cl.`Country/Region` = wd.`Country/Region`
+)
+SELECT 
+    `Country/Region`,
+    (Deaths / Population) * 100 AS Local_per 
+FROM joined_table;
+
+
+WITH joined_table AS (
+    SELECT 
+        cl.`Country/Region`, 
+        cl.Confirmed, 
+        cl.Deaths, 
+        cl.Recovered, 
+        cl.Active, 
+        cc.Date, 
+        wd.Population, 
+        wd.Continent
+    FROM country_wise_latest cl
+    INNER JOIN covid_19_clean_complete cc ON cl.`Country/Region` = cc.`Country/Region`
+    INNER JOIN full_grouped fg ON cl.`Country/Region` = fg.`Country/Region`
+    INNER JOIN worldometer_data wd ON cl.`Country/Region` = wd.`Country/Region`
+)
+SELECT 
+    (SUM(Deaths) / SUM(Population)) * 100 AS global_per
+FROM joined_table;
+
