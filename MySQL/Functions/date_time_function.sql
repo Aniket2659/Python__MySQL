42b8aeb270f7f6b4d7d44ade5cdf52a7d57c3cd0
Aniket2659 <aniket2659@gmail.com>
2024-08-07 16:11:39 +0530

    [Aniket] Add: this sql file contains date time queries

---
+++ MySQL/Functions/date_time_function.sql
@@ -0,0 +1,43 @@
+show databases;
+use bridgelabz;
+select * from Employee;
+
+-- now function
+select now();
+
+-- curdate function
+SELECT CURDATE();
+
+--  curtime function
+SELECT CURTIME();
+
+-- Date function
+SELECT DATE('2017-06-20 09:34:00');
+
+-- HOUR FUNCTION
+SELECT HOUR("2017-06-20 09:34:00");
+
+-- makedate function
+SELECT MAKEDATE(2017, 4);
+
+-- dayofmonth function
+SELECT DAYOFMONTH("2017-06-15");
+
+-- extract function
+SELECT EXTRACT(DAY FROM "2017-06-15");
+
+-- sec to time function
+SELECT SEC_TO_TIME(405);
+
+-- dayname function
+SELECT DAYNAME("2017-06-15");
+
+
+
+
+
+
+
+
+
+
