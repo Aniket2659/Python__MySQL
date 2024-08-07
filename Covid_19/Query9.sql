73613290033f4ea358628dd48ba7623530b9dd57
Aniket2659 <aniket2659@gmail.com>
2024-08-07 15:57:52 +0530

    [Aniket] Add: Problem of covid19 database : To find out the countries with the lowest death rates

---
+++ Covid_19/Query9.sql
@@ -0,0 +1,26 @@
+-- 9 To find out the countries with the lowest death rates
+
+use covid_19;
+
+WITH joined_table AS (
+    SELECT 
+        cl.`Country/Region`, 
+        cl.Confirmed, 
+        cl.Deaths, 
+        cl.Recovered, 
+        cl.Active, 
+        cc.Date, 
+        wd.Population, 
+        wd.Continent
+    FROM country_wise_latest cl
+    INNER JOIN covid_19_clean_complete cc ON cl.`Country/Region` = cc.`Country/Region`
+    INNER JOIN full_grouped fg ON cl.`Country/Region` = fg.`Country/Region`
+    INNER JOIN worldometer_data wd ON cl.`Country/Region` = wd.`Country/Region`
+)
+SELECT 
+    `Country/Region`, 
+    (Deaths / Confirmed) * 100 AS death_rate_percentage
+FROM joined_table
+WHERE Confirmed > 0
+ORDER BY death_rate_percentage ASC
+LIMIT 10;
